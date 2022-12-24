24de30b4c31060a35df588bfbaf2a90781acd2a7
Kadiralieva Madina <kadiralievamadina03@gmail.com>
2022-12-24 14:15:32 +0600

    fourth problem

---
+++ HW4/RecObj.js
@@ -0,0 +1,35 @@
+'use strict';
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+let inputString = '';
+let currentLine = 0;
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function Rectangle(a, b) {
+    let p=2*(a+b);
+    let area=a*b;
+    const rectangle={length:a, width:b, perimeter:p, area:area};
+    return rectangle;
+}
+
+function main() {
+    const a = +(readLine());
+    const b = +(readLine());
+    const rec = new Rectangle(a, b);
+    console.log(rec.length);
+    console.log(rec.width);
+    console.log(rec.perimeter);
+    console.log(rec.area);
+}
