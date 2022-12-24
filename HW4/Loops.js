451e53c83d478ed1fead7b6bb805661858c9a0e8
Kadiralieva Madina <kadiralievamadina03@gmail.com>
2022-12-24 13:02:23 +0600

    third problem

--- HW4/Loops.js
+++ HW4/Loops.js
@@ -1,11 +1,8 @@
 'use strict';
-
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
-
 let inputString = '';
 let currentLine = 0;
-
 process.stdin.on('data', inputStdin => {
     inputString += inputStdin;
 });
@@ -25,12 +22,12 @@ function readLine() {
 function vowelsAndConsonants(myStr) {
     for (let i=0; i<myStr.length; i++){
         if(myStr[i]=='i' || myStr[i]=='a' || myStr[i]=='e' || myStr[i]=='o' || myStr[i]=='u') {
-        console.log(myStr[i])
+        console.log(myStr[i]);
     }
     }
     for (let j=0; j<myStr.length; j++){
         if (myStr[j]!='i' && myStr[j]!='a' && myStr[j]!='e' && myStr[j]!='o' && myStr[j]!='u') {
-        console.log(myStr[j])
+        console.log(myStr[j]);
     }
 }
 }
