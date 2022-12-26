e1ec9575544697fb086d26d8e9858a615fbc00b9
Kadiralieva Madina <kadiralievamadina03@gmail.com>
2022-12-26 21:30:41 +0600

    draft

--- JavaScript/1.js
+++ JavaScript/1.js
@@ -1,18 +1,1 @@
-function vowelsAndConsonants(myStr) {
-    for (let i=0; i<myStr.length; i++){
-        if (myStr[i]=='i' || myStr[i]=='a' || myStr[i]=='e' || myStr=='o' || myStr=='u') {
-            console.log(myStr[i])
-        }
-    }
-    for (let i=0; i<myStr.length; i++){
-        if(myStr[i]!='i' || myStr[i]!='a' || myStr[i]!='e' || myStr!='o' || myStr!='u') {
-            console.log(myStr[i])
-        }
-    }
-}
-function main() {
-    const s = 'javascript';
-    vowelsAndConsonants(s);
-    len=s.length
-    console.log(len)
-}
+
